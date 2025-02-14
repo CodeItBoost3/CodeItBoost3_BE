bb44a855427f60b9b29d3a7aba382b7b5acaf264
wuseong <jws001228@naver.com>
2025-02-14 16:46:59 +0900

    🦄 Refactor: DB 작업 모듈화

---
+++ config/db.js
@@ -0,0 +1,10 @@
+export async function checkDBConnection() {
+  try {
+    await prisma.$connect();
+    console.log('✅ Database 연결 성공');
+  } catch (error) {
+    console.error('❌ Database 연결 실패:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
