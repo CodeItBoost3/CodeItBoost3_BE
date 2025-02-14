d1d58ad4a2ddcb52cc5d4b879a205f8318338450
wuseong <jws001228@naver.com>
2025-02-14 16:35:48 +0900

    ✨ Feat: bearer 토큰 검증 미들웨어 등록, 에러 핸들러 등록
    - 기본적으로 모든 요청에 대해서 검증을 실시하고 성공 시 req.user를 저장합니다.
    - 검증할 토큰이 없거나 검증 실패 시엔 에러를 발생시킵니다.
    - unless를 통해서 검증하지 않을 url을 등록합니다.
    - 에러 핸들러가 마지막에 위치하여 모든 에러에 대해서 상태 코드, 메시지를 만들어 실패 응답합니다.

--- app.js
+++ app.js
@@ -2,30 +2,43 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { PrismaClient, Prisma } from '@prisma/client';
-import { assert } from 'superstruct';
 import userRouter from './user/routes/user.js'
 import authRouter from './auth/routes/auth.js'
+import { errorHandler } from './error/error.js';
+import { authenticateByToken } from './auth/routes/authToken.js';
+import { checkDBConnection } from './config/db.js';
 
 export const prisma = new PrismaClient();
-
-
-async function checkDBConnection() {
-    try {
-        await prisma.$connect();
-        console.log('✅ Database 연결 성공');
-    } catch (error) {
-        console.error('❌ Database 연결 실패:', error);
-    } finally {
-        await prisma.$disconnect();
-    }
-}
 
 checkDBConnection();
 
 const app = express();
 app.use(express.json());
 
+
+// 비동기 에러를 에러 핸들러로 전해주기 위한 고차함수
+export function wrapAsync(fn){
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
+// 토큰 인증
+app.use(
+  wrapAsync(authenticateByToken.unless({
+    path: [
+      { url: "/users", methods: ["POST"] },  // 회원가입 제외
+      { url: "/users/validation", methods: ["GET"] }, // 아이디 유일성 검사 제외
+      { url: "/auth/login", methods: ["POST"] }, // 로그인 제외
+    ],
+  })
+));
+
+// 라우팅
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
+
+// 에러 핸들러(마지막에 위치 해야함)
+app.use(errorHandler());
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
