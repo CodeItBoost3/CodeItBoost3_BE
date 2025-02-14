53f93d0d7e492b146c94f558cda3d073953920a8
wuseong <jws001228@naver.com>
2025-02-14 16:50:37 +0900

    ✨Feat: 유저 업데이트 유효성 검증 객체 추가

--- user/userStructs.js
+++ user/userStructs.js
@@ -1,7 +1,9 @@
-import { object, string, size, min } from 'superstruct';
+import { object, string, size, partial } from 'superstruct';
 
 export const createUser = object({
   clientId: size(string(), 6, 15), // 6~15자 문자열
   password: size(string(), 8, 16), // 8~16자 문자열
   nickname: size(string(), 1, 30) // 1~30자 문자열
 });
+
+export const updateUser = partial(createUser);
